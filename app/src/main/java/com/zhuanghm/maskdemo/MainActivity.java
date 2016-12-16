b1a4fa4b3bf3587f6f78e7a98a486862e4d5ec5e
zhuanghm <zhuanghm@vastio.com>
2016-12-16 16:38:05 +0800

    Initial commit

---
+++ app/src/main/java/com/zhuanghm/maskdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhuanghm.maskdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
